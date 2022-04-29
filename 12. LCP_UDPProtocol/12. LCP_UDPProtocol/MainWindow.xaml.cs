9902e16b80441dfe1d750ce42bb99d74083e363e
Yoon SangHoon <54784919+KooEunBam@users.noreply.github.com>
2022-04-29 10:21:16 +0900

    Add files via upload

--- 12. LCP_UDPProtocol/12. LCP_UDPProtocol/MainWindow.xaml.cs
+++ 12. LCP_UDPProtocol/12. LCP_UDPProtocol/MainWindow.xaml.cs
@@ -28,14 +28,16 @@ namespace _12.LCP_UDPProtocol
         private readonly AutoResetEvent autoResetEvent;
         private readonly Queue<NewData> queue; // Queue 객체
         private readonly UdpClient udpClient; // UdpClient 객체
-        
-        
+
+
         private const int thread_sleep = 100; // Thread 주기
         private const int thread_runningtime = 10000;
 
+        
         private IPAddress ip_Address; // IP주소
         private IPEndPoint endpoint; // Port번호
         private byte[] data;
+
         private Thread th1;
         //private Thread th2;
 
@@ -48,13 +50,17 @@ namespace _12.LCP_UDPProtocol
             this.udpClient = new UdpClient(); // UdpClient 객체 생성
 
 
-            this.ip_Address = new IPAddress(long.Parse(IP_TextBox.Text)); // IP주소 객체 생성
-            this.endpoint = new IPEndPoint(ip_Address, int.Parse(Port_TextBox.Text)); // Port번호 생성
-            this.data = new byte[(udpClient.Receive(ref endpoint)).Length];
+            this.ip_Address = new IPAddress(long.Parse(IP_TextBox.Text)); // IP주소
+            this.endpoint = new IPEndPoint(ip_Address, int.Parse(Port_TextBox.Text)); // Port번호
+            this.data = new byte[(udpClient.Receive(ref endpoint)).Length]; 
+            // 수신 데이터와 함께 상대 컴퓨터의 종단점 정보도 같이 전달받는데,
+            // 이를 위해 IPEndPoint 객체를 ref파라미터로 전달함
+
             this.th1 = new Thread(new ThreadStart(thread1));
             //this.th2 = new Thread(new ThreadStart(thread2));
 
             InitializeComponent();
+
             th1.Start();
             //th2.Start();
         }
@@ -66,12 +72,14 @@ namespace _12.LCP_UDPProtocol
                 //data = udpClient.Receive(ref endpoint);
                 for (int i = 0; ;i++)
                 {
-                    if (data[i] != null)
+                    if (udpClient.Available > 0)
                     {
                         NewData newData = new NewData(i, data);
+                        queue.Enqueue(newData);
                     }
                     else
                     {
+                        udpClient.Close();
                         break;
                     }
                 }
